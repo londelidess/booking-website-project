afbce0188b9b2d5c1ada172eb93368af768b04e3
makoto <d.makoto0701@yahoo.com>
2023-06-13 10:43:23 -0700

    authorization

--- backend/utils/authorization.js
+++ backend/utils/authorization.js
@@ -1,4 +1,4 @@
-const { User } = require('../db/models');
+// const { User } = require('../db/models');
 
 const hasRole = (role) => {
     return function (req, res, next) {
@@ -23,10 +23,22 @@ const hasRole = (role) => {
     };
   };
 
+// Require proper authorization
+const requireProperAuthorization = (req, res, next) => {
+  if (!req.user) {
+      return res.status(403).json({ message: "Forbidden" });
+  }
+
+  if (!req.user.hasCorrectRoleOrPermission()) {
+      return res.status(403).json({ message: "Forbidden" });
+  }
+
+  next();
+};
 
   // in a routes, I use this like this
   // const express = require('express');
-  // const { requireAuth, requireRole, requirePermission } = require('path-to-your-auth-file');
+  // const { requireAuth, requireRole, requirePermission } = require('../');
   // const router = express.Router();
 
   // router.get('/some-protected-route', requireAuth, requireRole('admin'), requirePermission('read'), (req, res) => {
