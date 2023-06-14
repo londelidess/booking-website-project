7708c8b393e8d5d4890234de46fce2b272e2be36
makoto <d.makoto0701@yahoo.com>
2023-06-13 17:52:30 -0700

    debug model spot

--- backend/db/seeders/20230612212026-spot-data.js
+++ backend/db/seeders/20230612212026-spot-data.js
@@ -10,27 +10,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     options.tableName = 'Spots';
     await queryInterface.bulkInsert(options, [
-      //   {
 
-      //     address: '2-3-19 Asakusa',
-      //     city: 'Taito',
-      //     state: 'Tokyo',
-      //     country: 'Japan',
-      //     lat: 35.71365872529517,
-      //     lng: 139.7942737932121,
-      //     description: 'Central Asakusa',
-      //     price: 100.00,
-      // },
-      // {
-      //   address: '262 Lac Long Quan, Phuong 5, Quan 11',
-      //   city: 'Thanh pho',
-      //   state: 'Ho Chi Minh',
-      //   country: 'Vietnam',
-      //   lat: 10.76651264051985,
-      //   lng: 106.64192355654245,
-      //   description: 'next to Dam Sen: old popular theme park in Saigon ',
-      //   price: 50.00,
-      // },
       {
         address: "6 E Aspen Ave # 200",
         city: "Flagstaff",
@@ -46,6 +26,6 @@ module.exports = {
 
   async down(queryInterface, Sequelize) {
     options.tableName =  'Spots';
-    return queryInterface.bulkDelete(options, null, {});
+    return await queryInterface.bulkDelete(options, null, {});
   },
 };
