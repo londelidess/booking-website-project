8779cdbabc7fddbf8f09a702b844db46c25b3c9e
makoto <d.makoto0701@yahoo.com>
2023-06-30 14:42:02 -0700

    Modals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
-import configureStore from './store';
-
 // import { createRoot } from 'react-dom/client';///
+import React from "react";
 
-import { restoreCSRF, csrfFetch } from './store/csrf';
+import "./index.css";
 
-import * as sessionActions from './store/session';
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ModalProvider, Modal } from "./context/Modal";
+import App from "./App";
+
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
+import * as sessionActions from "./store/session";
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   restoreCSRF();
 
   window.csrfFetch = csrfFetch;
   window.store = store;
   window.sessionActions = sessionActions;
 }
-// console.log(sessionActions);
 
+// Wrap the application with the Modal provider and render the Modal component
+// after the App component so that all the Modal content will be layered as
+// HTML elements on top of the all the other HTML elements:
 function Root() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </Provider>
+    </ModalProvider>
   );
 }
 
@@ -39,7 +43,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 // const root = createRoot(document.getElementById('root'));
